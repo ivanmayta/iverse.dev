a57156ba97b83e0534e948c0b1c2e257b62ac8c9
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-16 22:10:20 -0500

    ✳️add Icons

---
+++ src/icons/Jisoo.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+export default function Jisoo() {
+    return (
+        <Image
+            src="/image/jisoo.webp"
+            alt="Jisoo Lover"
+            width="727"
+            height="727"
+        ></Image>
+    )
+}
---
+++ src/icons/Next.jsx
@@ -0,0 +1,19 @@
+export default function Next(){
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5l7 7-7 7"
+            />
+        </svg>
+    )
+}
