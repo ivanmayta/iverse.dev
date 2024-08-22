7232db6db5e85cf716af3672303d9efd83e2234c
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-22 18:46:03 -0500

    ✳️Add icons

---
+++ src/icons/Kotlin.jsx
@@ -0,0 +1,12 @@
+export const Kotlin = (props) => {
+    return (
+        <svg
+            class="svg-icon"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path fill="currentColor" d="M55.467 1024 537.6 533.333 1024 1024zM0 0h512L0 533.333zm571.733 0L0 597.333V1024l512-512L1024 0z"></path>
+        </svg>
+    )
+}
