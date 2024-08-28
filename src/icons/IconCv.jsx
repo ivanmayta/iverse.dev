9e6605f2922a43960986a39b95d61221df135097
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-28 17:30:07 -0500

    🖼️fix icons dimentions

--- src/icons/IconCv.jsx
+++ src/icons/IconCv.jsx
@@ -4,8 +4,8 @@ export function IconCv(props) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="1rem"
-            height="1rem"
+            width="24"
+            height="24"
             viewBox="0 0 20 20"
             {...props}
         >
--- src/icons/IconLinkedIn.jsx
+++ src/icons/IconLinkedIn.jsx
@@ -4,8 +4,8 @@ export function IconLinkedIn(props) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="1rem"
-            height="1rem"
+            width="24"
+            height="24"
             viewBox="0 0 15 15"
             {...props}
         >
