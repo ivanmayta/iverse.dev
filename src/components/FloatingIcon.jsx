8289dc7129b88a1d22fb49853a17100a99064a2f
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-28 17:49:26 -0500

    ✳️Floating Icon Component

--- src/components/FloatingIcon.jsx
+++ src/components/FloatingIcon.jsx
@@ -3,21 +3,22 @@ import Link from "next/link"
 
 export function FloatingIcon({ profiles }) {
     return (
-        <ul className="flex gap-x-2 z-40">
+        <ul className="flex z-40">
             {profiles.map((profile) => {
                 return (
                     <li
-                        className=" hover:bg-neutral-900 p-2  rounded-sm "
+                        className=" hover:bg-neutral-900 rounded-sm group p-2 hover:transition-all relative "
                         key={profile.network}
                     >
                         <Link
                             key={profile.network}
                             href={profile.url}
                             target="_blank"
-                            className="flex items-center justify-center rounded-full  shadow-lg box-content text-current sm:size-7 md:size-8  hover:text-accent-foreground/80"
+                            className="flex shadow-lg text-current sm:size-7 md:size-8  hover:text-accent-foreground/80"
                         >
                             {profile.icon}
                         </Link>
+                        <span className="absolute top-full -inset-1 transition-all scale-0 rounded bg-background p-2 text-xs text-white group-hover:scale-100 text-center">{profile.network}</span>
 
                         
                     </li>
