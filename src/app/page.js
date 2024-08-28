b70cb6d4518f24517690e5ddf90ff579ca3cd202
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-28 17:49:06 -0500

    ✳️page update

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,15 @@ import Projects from "@/components/Projects"
 
 export default function Home() {
     return (
-        <main className="z-0  space-y-36 pt-16 lg:space-y-48">
-            <Hero className="" />
-            <Projects
+        <main className={` space-y-36 pt-16 lg:space-y-48`}>
+            <Hero />
+            {/*<Projects2
                 id="proyectos"
                 className="mx-auto max-w-screen-lg content-center px-4 xs:px-6 sm:px-8 "
+            />*/}
+            <Projects
+                className="mx-auto max-w-screen-lg content-center px-4 xs:px-6 sm:px-8"
+                title="Proyectos"
             />
         </main>
     )
