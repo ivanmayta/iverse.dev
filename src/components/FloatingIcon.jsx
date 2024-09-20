66106d72f365ccdd1362aa10cf5f98f7cfa3b055
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-09-19 20:35:02 -0500

    ✳️Refactor: styles to more accessibility

--- src/components/FloatingIcon.jsx
+++ src/components/FloatingIcon.jsx
@@ -3,24 +3,24 @@ import Link from "next/link"
 
 export function FloatingIcon({ profiles }) {
     return (
-        <ul className="flex z-40">
+        <ul className="flex">
             {profiles.map((profile) => {
                 return (
                     <li
-                        className=" hover:bg-neutral-900 rounded-sm group p-2 hover:transition-all relative "
+                        className="relative hover:bg-neutral-800 rounded-lg group p-2 hover:transition-all"
                         key={profile.network}
-                    >
+                    >   
                         <Link
                             key={profile.network}
                             href={profile.url}
                             target="_blank"
-                            className="flex shadow-lg text-current sm:size-7 md:size-8  hover:text-accent-foreground/80"
+                            className="flex shadow-lg text-current "
                         >
                             {profile.icon}
                         </Link>
-                        <span className="absolute top-full -inset-1 transition-all scale-0 rounded bg-background p-2 text-xs text-white group-hover:scale-100 text-center">{profile.network}</span>
-
-                        
+                        <span className="absolute top-full -inset-1 transition-all scale-0 rounded bg-background text-xs text-slate-300 group-hover:scale-100 text-center">
+                            {profile.network}
+                        </span>
                     </li>
                 )
             })}
