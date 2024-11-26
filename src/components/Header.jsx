7dfc1df391ed28b4ace6858f2c067c4f1897a080
Ivan Rodrigo <ivan.rodrigo.mayta@gmail.com>
2024-11-26 16:58:16 -0500

    style: improve header link styling with group hover effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,10 @@ export default function Header() {
     return (
         <header className="  w-full  mt-2  inset-x-0 sticky top-0 z-50 overflow-x-auto fold:overflow-x-clip">
             <nav className="flex w-full max-w-3xl  mx-auto items-center justify-between  py-4 backdrop-blur-sm  text-sm font-medium text-gray-600 dark:text-gray-200">
-                <a href="/" className="flex justify-center items-center gap-2">
+                <a
+                    href="/"
+                    className="flex group justify-center items-center gap-2"
+                >
                     <img
                         className="aspect-square size-9  overflow-clip rounded-full  object-cover"
                         src={image}
@@ -18,7 +21,7 @@ export default function Header() {
                     />
                     <p className="flex flex-col">
                         <span className="font-bold text-base">iverse.dev</span>
-                        <span className="text-xs text-neutral-400">
+                        <span className="text-xs opacity-60 group-hover:opacity-75 ">
                             @iversedev
                         </span>
                     </p>
