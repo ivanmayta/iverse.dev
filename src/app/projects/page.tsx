1b9362ccfc91ced5db9c16ed0eae8290b1f4a6af
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-12-27 11:13:24 -0500

    feat: update Projects component header text for clarity

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,7 @@
 export default function Projects() {
     return (
         <section className="w-full  max-w-3xl mx-auto flex items-center justify-center">
-            <h1 className="text-5xl font-semibold">
-                I'm just a chill developer
-            </h1>
+            <h1 className="text-5xl font-semibold">I&apos;m a developer.</h1>
             <img src="/chill-guy.webp" alt="Chill Guy Developer" />
         </section>
     )
