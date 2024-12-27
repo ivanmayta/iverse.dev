0304fec6b17c75af324319028b0a97bea5e8c3c1
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-12-27 01:51:51 -0500

    feat: update Projects component with new layout and image

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,10 @@
 export default function Projects() {
     return (
-        <section className="w-full h-64 max-w-3xl mx-auto">
-            <h1>🙆‍♂️ Luego lo hago... </h1>
+        <section className="w-full  max-w-3xl mx-auto flex items-center justify-center">
+            <h1 className="text-5xl font-semibold">
+                I'm just a chill developer
+            </h1>
+            <img src="/chill-guy.webp" alt="Chill Guy Developer" />
         </section>
     )
 }
