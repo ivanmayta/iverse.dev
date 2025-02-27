db8668589401daf6580f6e6fc43369487cf1cd20
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-02-26 23:17:33 -0500

    feat: add Navigation component for improved site navigation

---
+++ src/components/ui/navigation.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { GitHub } from "@/icons/socials"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+import path from "path"
+const navs = [
+    { href: "/", label: "Home" },
+    { href: "/projects", label: "Projects" },
+    { href: "/posts", label: "posts" },
+]
+export default function Navigation() {
+    const pathname = usePathname()
+    const normalizePath = (path: string) => {
+        const segments = path.split("/")
+        return segments.length > 1 ? `/${segments[1]}` : "/"
+    }
+    console.log(normalizePath(pathname))
+    console.log(pathname)
+    const isActive = (href: string) => {
+        return normalizePath(pathname) === normalizePath(href)
+    }
+    console.log(isActive(navs[1].href))
+    return (
+        <nav className="flex gap-4">
+            {navs.map(({ href, label }) => (
+                <Link
+                    className={cn({
+                        "font-semibold text-[#9d4cfa] underline":
+                            isActive(href),
+                        "text-gray-600 dark:text-gray-200 hover:underline":
+                            !isActive(href),
+                    })}
+                    key={href}
+                    href={href}
+                >
+                    {label}
+                </Link>
+            ))}
+            <a
+                className="flex gap-1 items-center text-gray-600 dark:text-gray-200"
+                href="https://github.com/ivanmayta/iverse.dev"
+                target="_blank"
+            >
+                <GitHub />
+                Source
+            </a>
+        </nav>
+    )
+}
