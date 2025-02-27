c0e8efef18dd5568cbf9ff2db452cf077741a6a9
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-02-26 23:59:46 -0500

    refactor: improve TooltipStatus styles and optimize GitHub icon SVG structure

--- src/components/ui/tooltip-status.tsx
+++ src/components/ui/tooltip-status.tsx
@@ -4,9 +4,9 @@ interface TooltipStatusProps {
 }
 export default function TooltipStatus({ icon, text }: TooltipStatusProps) {
     return (
-        <div className="group relative">
+        <div className="group relative text-xs">
             {icon}
-            <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-44 border transition-all scale-0 rounded bg-background text-xs text-slate-300 group-hover:scale-100 text-center p-1">
+            <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 w-44 border text-xs transition-all scale-0 rounded bg-background  text-slate-300 group-hover:scale-100 text-center p-1">
                 {text}
             </span>
         </div>
