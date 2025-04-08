6b842fdb43c65850a6ad6bd5b3a9d78329b6758a
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-04-08 12:00:42 -0500

    feat: adjust FlickeringGrid

--- src/components/flickering-grid.tsx
+++ src/components/flickering-grid.tsx
@@ -4,10 +4,10 @@ export function FlickeringGridBackGround() {
     return (
         <FlickeringGrid
             className="-z-10 absolute inset-0 size-full min-h-screen"
-            squareSize={0.9}
-            gridGap={7}
+            squareSize={0.5}
+            gridGap={8}
             color="#6B7280"
-            maxOpacity={0.5}
+            maxOpacity={1}
             flickerChance={2}
         />
     )
